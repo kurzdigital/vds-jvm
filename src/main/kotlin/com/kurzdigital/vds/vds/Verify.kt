b5a0aaa357524818746b6dd71a40c61b05b62010
Markus Fisch <markus.fisch@kurzdigital.com>
2023-05-19 15:45:30 +0200

    Add a method to verify a VDS with a single cert
    Because this is very common use case and we shouldn't need
    to set up a CertificateIterator when we don't need it.

--- src/main/kotlin/com/kurzdigital/vds/vds/Verify.kt
+++ src/main/kotlin/com/kurzdigital/vds/vds/Verify.kt
@@ -1,6 +1,8 @@
 package com.kurzdigital.vds.vds
 
 import com.kurzdigital.vds.security.CertificateIterator
+import com.kurzdigital.vds.security.validAt
+import java.security.cert.Certificate
 import java.util.Date
 
 fun Vds.verify(
@@ -11,4 +13,13 @@ fun Vds.verify(
     sha256,
     signature,
     date
+)
+
+fun Vds.verify(
+    certificate: Certificate,
+    date: Date = Date()
+) = certificate.validAt(date) && com.kurzdigital.vds.security.verify(
+    certificate.publicKey,
+    sha256,
+    signature
 )
