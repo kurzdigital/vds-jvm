83c07e4862decf8705c1aa091691b5d1dd5fccc9
Markus Fisch <markus.fisch@kurzdigital.com>
2023-10-04 09:09:16 +0200

    Update CSCA MasterList

--- src/test/kotlin/com/kurzdigital/vds/vdsnc/VdsNcTests.kt
+++ src/test/kotlin/com/kurzdigital/vds/vdsnc/VdsNcTests.kt
@@ -19,7 +19,7 @@ class VdsNcTests {
         }
         trustAnchors = ByteArrayInputStream(
             ClassLoader.getSystemResource(
-                "20230831_DEMasterList.ml",
+                "20230927_DEMasterList.ml",
             ).readBytes(),
         ).readCscaMasterList()
     }
